9c3af71b7975eaff3b81d8d896a194a2ca1d2a51
nayakrujul <nayakrujul@hotmail.com>
2024-02-24 18:10:57 +0000

    Update the workflow

---
+++ helpers.py
@@ -0,0 +1,26 @@
+import re
+import os
+
+ld = os.listdir("revision/")
+
+def rreplace(s, old, new):
+    li = s.rsplit(old, 1) #Split only once
+    return new.join(li)
+
+with open("revision/index.html", "r", encoding='utf-8') as e:
+    t = e.read()
+    lst = re.findall(r'<a href="\./(.*)/">', t)
+    for s in ld:
+        if ("." not in s) and (s[0].isdigit()) and (s not in lst):
+            t = rreplace(t, "</ul>", f"""    <li><a href="./{s}/">{s.replace('-', '.', 1).replace('-', chr(92), 1).replace('-', ' ').replace(chr(92), ' - ')}</a></li>
+        </ul>""")
+
+with open("revision/index.html", "w") as f:
+    f.write(t)
+
+with open("sitemap.txt", "r", encoding='utf-8') as g, \
+        open("sitemap.txt", "a") as h:
+    sitemap = re.findall(r'revision/(.*)/', g.read())
+    for string in lst:
+        if string not in sitemap:
+            h.write("\nhttps://cs.rujulnayak.com/revision/" + string + "/")
