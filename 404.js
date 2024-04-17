bb7b91c0b1485d0192d68e6782c3e1a0311deaff
nayakrujul <nayakrujul@hotmail.com>
2024-04-17 15:29:13 +0100

    Final fix

--- 404.js
+++ 404.js
@@ -2,7 +2,7 @@ const LATEST = "v1";
 
 let url = location.href;
 
-let matches = [...url.matchAll(/.*\/revision\/(.*)\/?(?:index.html)?/gi)];
+let matches = [...url.matchAll(/.*\/revision\/?(.*)\/?(?:index.html)?/gi)];
 
 if (matches.length > 0) {
     let x = matches[0][1];
