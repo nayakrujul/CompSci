ebc11c042c8befcfba1c2bbb4402853577d80642
nayakrujul <nayakrujul@hotmail.com>
2024-04-17 17:54:50 +0100

    Last one I promise

--- 404.js
+++ 404.js
@@ -7,6 +7,6 @@ let matches = [...url.matchAll(/.*\/revision\/?(.*)\/?(?:index.html)?/gi)];
 if (matches.length > 0) {
     let x = matches[0][1];
     location.replace(`https://cs.rujulnayak.com/${LATEST}/${x}`);
+} else {
+    document.getElementById("heading").innerHTML = "Error 404 - Page Not Found";
 }
-
-document.getElementById("heading").innerHTML = "Error 404 - Page Not Found";
