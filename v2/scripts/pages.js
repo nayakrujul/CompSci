7f6a892ad4cd6f380374cf9f9c7bf4778f1152b4
nayakrujul <nayakrujul@hotmail.com>
2024-07-22 13:19:53 +0100

    First page

---
+++ v2/scripts/pages.js
@@ -0,0 +1,22 @@
+const THEMES = ["blue", "green", "purple"];
+let rand = THEMES[Math.floor(Math.random() * 3)];
+document.getElementById("page").classList.add(rand);
+
+[...document.querySelectorAll(".summary-question")].map(qs => {
+    qs.innerHTML += `
+        <input type="button" class="q-button" value="Reveal answer" />
+    `;
+    qs.querySelector(".answer").innerHTML += "<br />&nbsp;";
+    qs.querySelector(".q-button").addEventListener("click", (event) => {
+        let el = event.target;
+        let pe = el.parentElement;
+        let ans = pe.querySelector(".answer");
+        if ([...ans.classList].includes("show")) {
+            ans.classList.remove("show");
+            el.value = "Reveal answer";
+        } else {
+            ans.classList.add("show");
+            el.value = "Hide answer";
+        }
+    })
+});
