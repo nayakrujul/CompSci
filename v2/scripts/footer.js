80d7f4b50f91f8b279d3ff09b064db98b7f4b233
nayakrujul <nayakrujul@hotmail.com>
2024-07-24 10:17:16 +0100

    New page

--- v2/scripts/footer.js
+++ v2/scripts/footer.js
@@ -1,2 +1,2 @@
 let year = (cy = new Date().getFullYear()) !== 2024 ? "-" + ("" + cy).slice(-2) : "";
-document.getElementById("footer").innerHTML = `&copy; Rujul Nayak 2024${year}`;
+document.getElementById("footer").innerHTML = `<b class="no-underline">&copy; Rujul Nayak 2024${year}</b>`;
