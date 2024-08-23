086a6ae4f4adef857468334eba26ae0ee46e993c
nayakrujul <nayakrujul@hotmail.com>
2024-08-23 11:05:21 +0100

    Finish the new homepage

--- v2/scripts/homepage.js
+++ v2/scripts/homepage.js
@@ -1,15 +1,6 @@
 const carousel = document.getElementById("homepage-carousel");
 const items = [...carousel.querySelectorAll("div.carousel-item")];
 
-items.forEach(item => {
-    item.innerHTML += `
-        <input type="button" class="q-button" value="Next"> <br />
-        <a class="explore-link" href="./v2">Explore the index! &rarr;</a>
-    `;
-
-    item.querySelector("input.q-button").addEventListener("click", next_item);
-})
-
 let active = -1;
 
 function next_item() {
@@ -24,3 +15,5 @@ function next_item() {
 }
 
 next_item();
+
+document.getElementById("next-button").addEventListener("click", next_item);
