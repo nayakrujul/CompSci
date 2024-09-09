5af3a49cf811f07877f63b02f609d957c7f5ea55
nayakrujul <nayakrujul@hotmail.com>
2024-09-09 18:00:15 +0100

    New page

---
+++ v2/9/01/main.js
@@ -0,0 +1,6 @@
+const today = new Date();
+
+document.getElementById("js-target-0").innerHTML = "today";
+document.getElementById("js-target-1").innerHTML = today.getFullYear();
+document.getElementById("js-target-2").innerHTML = today.getMonth() + 1;
+document.getElementById("js-target-3").innerHTML = today.getDate();
