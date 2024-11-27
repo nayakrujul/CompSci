1ce2cc7dc2aac69382c1ec29a0a55f0743522697
nayakrujul <nayakrujul@hotmail.com>
2024-11-27 21:12:36 +0000

    New section

---
+++ v2/16/01/main.js
@@ -0,0 +1,16 @@
+const t0 = document.getElementById("js-target-0");
+const t1 = document.getElementById("js-target-1");
+const t2 = document.getElementById("js-target-2");
+
+let curr = 1;
+let max = 71;
+
+t1.addEventListener("click", () => {
+    curr = (curr - 1) || max;
+    t0.src = "images/Slide" + curr + ".png";
+});
+
+t2.addEventListener("click", () => {
+    curr = (curr % max) + 1;
+    t0.src = "images/Slide" + curr + ".png";
+});
